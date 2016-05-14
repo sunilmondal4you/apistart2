d0fff631e5400c40c32d70ece73ce953ee55e674
viveking <vivekk252@gmail.com>
2016-05-14 17:46:55 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by abc on 14/05/2016.
+ */
+
+var express= require("express");
+
+var app= new express();
+
+
+
+app.listen(3000, function () {
+    console.log("Server Started");
+});
