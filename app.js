9c875749ba4b52565da3dedb74fea2a879e5406d
viveking <vivekk252@gmail.com>
2016-05-14 18:00:19 +0530

    change3

--- app.js
+++ app.js
@@ -14,8 +14,28 @@ app.get("/a", function(req, res){
     res.send("We wellcome...");
 });
 
+app.get("/b", function(req, res){
+    res.send("what is your Name?");
+});
+
+app.get("/a/b", function(req, res){
+    res.send("Where are you from?");
+});
+
+app.get("/a/b/c", function(req, res){
+    res.send("what are you doing?");
+});
+
 app.post("/a", function(req, res){
     res.send("We wellcome you.");
+});
+
+app.post("/b", function(req, res){
+    res.send("My name is Sunil");
+});
+
+app.post("/a/b", function(req, res){
+    res.send("I am from West Bengal.");
 });
 
 app.listen(3000, function () {
