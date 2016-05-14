3d26d7684fc76a928a3f1f92028951d5cf1e02df
viveking <vivekk252@gmail.com>
2016-05-14 17:50:42 +0530

    first change

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ var express= require("express");
 
 var app= new express();
 
+app.get("/", function(req, res){
+    res.send("Hello");
+});
 
+app.get("/a", function(req, res){
+    res.send("We wellcome...");
+});
 
 app.listen(3000, function () {
     console.log("Server Started");
